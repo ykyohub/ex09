62ee6315d80ee2b8044e3b1267ddf5529b660198
ykyohub <rbgh2858@naver.com>
2023-04-29 11:35:10 +0900

    ver02

--- src/components/LocalPage.jsx
+++ src/components/LocalPage.jsx
@@ -1,46 +1,81 @@
-import React, { useState } from 'react'
-import axios from 'axios'
-import { useEffect } from 'react'
-import {Row, Col, Table} from 'react-bootstrap'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { Col, Form, Row, Table, Button } from 'react-bootstrap';
+import MapPage from './MapPage';
 
 const LocalPage = () => {
     const [locals, setLocals] = useState([]);
+    const [query, setQuery] = useState('인하대학교')
+    const [total, setTotal] = useState(0);
+    const [page, setPage] = useState(1);
+    const [is_end, setIs_end] = useState(false);
+
     const getLocal = async() => {
-        const url = "http://dapi.kakao.com/v2/local/search/keyword.json";
-        const config = {
-            headers : {"Authorization":"KakaoAK b80880fbde422de3fd9b4a4e67c9bb54"},
-            params : {query : '인하대학교', page:1, size:5}
+        const url="https://dapi.kakao.com/v2/local/search/keyword.json";
+        const config={
+            headers: {"Authorization": "KakaoAK b80880fbde422de3fd9b4a4e67c9bb54"},
+            params: {query: query, page:page, size:5}
         }
         const result = await axios.get(url, config);
         console.log(result);
         setLocals(result.data.documents);
+        setTotal(result.data.meta.pageable_count);
+        setIs_end(result.data.meta.is_end);
     }
+
     useEffect(()=>{
         getLocal();
-    },[]);
+        
+    },[page]) //의존배열
 
+    const onSearch = (e) => {
+        e.preventDefault();
+        getLocal(); 
+    }
     return (
-        <div>
-            <h1 className='text-center mx-5'>지역검색</h1>
-            <Table striped bordered hover> 
-                <thead>
-                    <tr className = 'text-center'>
-                        <td>장소명</td>
-                        <td>주소</td>
-                        <td>전화번호</td>
-                    </tr>
-                </thead>
-                <tbody>
-                    {locals.map(local=>
-                        <tr key={local.id}>
-                            <td>{local.place_name}</td>
-                            <td>{local.address_name}</td>
-                            <td>{local.phone}</td>
+        <Row>
+            <Col>
+                <h1 className='text-center my-5'>지역검색</h1>
+                <Row>
+                    <Col md={3} xs={6}>
+                        <Form onSubmit={onSearch}>
+                            <Form.Control value = {query}
+                            onChange = {(e)=>setQuery(e.target.value)}
+                             placeholder='검색어' />
+                        </Form>
+                    </Col>
+                    <Col>검색수 : {total}</Col>
+                </Row>
+                <Table>
+                    <thead>
+                        <tr>
+                            <td>장소명</td>
+                            <td>전화번호</td>
+                            <td>주소</td>
+                            <td>위치</td>
                         </tr>
-                    )}
-                </tbody>
-            </Table>
-        </div>
+                    </thead>
+                    <tbody>
+                        {locals.map(local=>
+                            <tr key={local.id}>
+                                <td>{local.place_name}</td>
+                                <td>{local.phone}</td>
+                                <td>{local.address_name}</td>
+                                <td><MapPage local = {local}/></td>
+                            </tr>)}
+                    </tbody>
+                </Table>
+                <div className='text-center my-2'>
+                    <Button 
+                        disabled={page==1}
+                        onClick={()=>setPage(page-1)}>이전</Button>
+                    <span className='mx-3'>{page}</span>
+                    <Button 
+                        disabled={is_end==1}
+                        onClick={()=>setPage(page+1)}>다음</Button>
+                </div>
+            </Col>            
+        </Row>
     )
 }
 
